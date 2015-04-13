9e2758417c5a2cbf1af96dabec51a039709e4f2b
Wooleners <wooleners@gmail.com>
2015-04-13 19:01:34 +0800

    push

---
+++ alt.js
@@ -0,0 +1,7 @@
+var Alt = require('alt');
+var alt = new Alt();
+var chromeDebug = require('alt/utils/chromeDebug')
+
+chromeDebug(alt);
+
+module.exports = alt;
---
+++ app.js
@@ -0,0 +1,6 @@
+var React = require('react');
+var Locations = require('./components/Locations.jsx');
+
+React.render( < Locations / > ,
+	document.body
+);
